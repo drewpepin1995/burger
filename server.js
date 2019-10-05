fa71aeeb80ab772c0b44a82e700855ca2e294f2c
Drew Pepin <drewpepin1995@gmail.com>
2019-10-05 14:22:25 -0400

    setting up server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
