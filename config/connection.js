466eecb746b0734818c9ceb2072438182e772fe3
Drew Pepin <drewpepin1995@gmail.com>
2019-10-06 18:49:54 -0400

    Fixing heroku issue

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection(process.env.DATABASE_URL);
+var connection = mysql.createPool(process.env.DATABASE_URL);
 
 
 connection.connect(function(err) {
