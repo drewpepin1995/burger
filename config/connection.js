5d1eda44ef5ece591ee48042aeb9e55564783b6b
Drew Pepin <drewpepin1995@gmail.com>
2019-10-06 19:10:22 -0400

    heroku issue

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,6 @@ var mysql = require("mysql");
 var connection = mysql.createPool(process.env.DATABASE_URL);
 
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
 
 module.exports = connection;
