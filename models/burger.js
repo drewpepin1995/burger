16626c0704a716a4c54c01d8d97b68f4c4f69537
Drew Pepin <drewpepin1995@gmail.com>
2019-10-05 21:52:03 -0400

    Working on handlebars

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+    insertOne: function (valOfCol, valOfOtherCol, cb) {
+        orm.insertOne("burgers", valOfCol, valOfOtherCol, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (columnInput, condition, cb) {
+        orm.updateOne("burgers", columnInput, condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
